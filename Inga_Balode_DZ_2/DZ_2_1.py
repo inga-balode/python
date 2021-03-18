33cffdbd14c963dd0b8c26e720f40ae835187c71
inga-balode <ingaa.balode@gmail.com>
2021-03-18 23:15:03 +0200

    DZ_2

---
+++ Inga_Balode_DZ_2/DZ_2_1.py
@@ -0,0 +1,17 @@
+#. Выяснить тип результата выражений:
+# 15 * 3
+# 15 / 3
+# 15 // 2
+# 15 ** 2
+
+a = 15 * 3
+b = 15 / 3
+c = 15 // 2
+d = 15 ** 2
+
+print(type(a))
+print(isinstance(b, float))
+print(type(c) == int)
+print(type(d))
+
+
